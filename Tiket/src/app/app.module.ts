9d0a7d24bb6ea2b3f0a79957f9311a39f732a2cb
Perciva <Perciva@users.noreply.github.com>
2019-12-28 13:37:17 +0700

    header-not-complete

--- Tiket/src/app/app.module.ts
+++ Tiket/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './nav/header/header.component';
+import { FooterComponent } from './nav/footer/footer.component';
+import { FooterTopComponent } from './nav/footer-top/footer-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FooterTopComponent
   ],
   imports: [
     BrowserModule,
